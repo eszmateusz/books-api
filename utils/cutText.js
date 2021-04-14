4864f6fa0dcde69b90e77b21cfe1b4be02e2ee3f
eszmateusz <71881067+eszmateusz@users.noreply.github.com>
2021-04-14 21:07:15 +0200

    Code formatting improve.

--- utils/cutText.js
+++ utils/cutText.js
@@ -1,10 +1,27 @@
 module.exports = (content, maxLength) => {
-  if(typeof maxLength !== 'number') return 'Error';
-  if(maxLength <= 0) return 'Error';
 
-  if(typeof content !== 'string') return 'Error';
+  if (typeof maxLength !== 'number') {
+    return 'Error';
+  }
 
-  if(content.length < 1) return 'Error';
-  if(content.length <= maxLength) return content;
-  return content.substr(0, content.lastIndexOf(' ', maxLength)) + '...';
+  if (maxLength <= 0) {
+    return 'Error';
+  }
+
+  if (typeof content !== 'string') {
+    return 'Error';
+  }
+
+  if (content.length < 1) {
+    return 'Error';
+  }
+
+  if (content.length <= maxLength) {
+    return content;
+  }
+  
+  else {
+    return content.substr(0, content.lastIndexOf(' ', maxLength)) + '...';
+  }
+
 };
--- utils/formatFullname.js
+++ utils/formatFullname.js
@@ -1,7 +1,20 @@
 module.exports = (fullName) => {
-  if(typeof fullName !== 'string') return 'Error';
+
+  if (typeof fullName !== 'string') {
+    return 'Error';
+  }
+
   const splittedFullname = fullName.split(' ');
-  const [ firstName, lastName ] = splittedFullname;
-  if(!firstName || !lastName || splittedFullname.length > 2) return 'Error';
-  return firstName[0].toUpperCase() + firstName.slice(1).toLowerCase() + ' ' + lastName[0].toUpperCase() + lastName.slice(1).toLowerCase();
+  const [firstName, lastName] = splittedFullname;
+
+  if (!firstName || !lastName || splittedFullname.length > 2) {
+    return 'Error';
+  }
+
+  else {
+    return firstName[0].toUpperCase() + firstName.slice(1).toLowerCase() +
+    ' ' + 
+    lastName[0].toUpperCase() + lastName.slice(1).toLowerCase();
+  }
+
 };
